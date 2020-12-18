b20b2db6b94631f3a030fb6dd8f6ca23488b90dc
Sean Francis N. Ballais <sfballais123@gmail.com>
2020-12-18 20:11:57 +0800

    :construction: Add code that plays with QGIS's Python API.

--- bpt_qgis/bpt_qgis.py
+++ bpt_qgis/bpt_qgis.py
@@ -22,9 +22,9 @@
  ***************************************************************************/
 """
 import os
-import sys
 
 import pylibbpt as bpt
+from qgis.core import QgsProject
 from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtWidgets import QAction
@@ -184,8 +184,13 @@ class BPTQGIS:
 
     def run(self):
         """Run method that performs all the real work"""
-        ga = bpt.GA()
-        print(ga.getCurrentRunGenerationNumber())
+        for layer in QgsProject.instance().mapLayers().values():
+            print(layer.name())
+            print(type(layer))
+            features = layer.getFeatures()
+
+            for feature in features:
+                print(feature.geometry().asMultiPolygon())
 
         # Create the dialog with elements (after translation) and keep reference
         # Only create GUI ONCE in callback, so that it will only load when the plugin is started
